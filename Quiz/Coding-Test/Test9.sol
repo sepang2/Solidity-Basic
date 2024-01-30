29bee14cd5859191e00161ae8238e46e7f86c07d
Sepang2 <sepang296@gmail.com>
2024-01-30 18:06:28 +0900

    Update Test9

--- Quiz/Coding-Test/Test9.sol
+++ Quiz/Coding-Test/Test9.sol
@@ -13,6 +13,7 @@ pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+// 짧게 준다고 쉬운 문제가 아니다.
 contract Test9_240124 {
     function depositForTest() public payable {}
 
@@ -42,6 +43,79 @@ contract Test9_240124 {
             payable(msg.sender).transfer(0.1 ether);
         } else {
             payable(msg.sender).transfer(0);
+        }
+    }
+}
+
+contract Test9_Answer {
+    address owner;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    struct lotto {
+        uint round;
+        string number;
+    }
+
+    mapping(address => lotto[]) public numbers;
+
+    lotto[] public target;
+    
+    function setTarget(string memory _s) public {
+        require(msg.sender == owner, "OWNER ONlY");
+        require(bytes(_s).length == 6);
+        target.push(lotto(target.length + 1, _s));
+    } 
+
+    function pickLotto(string memory _pick) public payable {
+        require(msg.value == 0.05 ether, "pay 0.05ETH please");
+        if(numbers[msg.sender].length > 0 && numbers[msg.sender][0].round == target.length) {
+            delete numbers[msg.sender];
+        }
+        numbers[msg.sender].push(lotto(target.length + 1, _pick));
+    }
+
+    function withdraw() public {
+        require(bytes(target[target.length].number).length > 0 && numbers[msg.sender][0].round == target.length, "Winning Numbers don't exist yet");
+        payable(msg.sender).transfer(check());
+        delete numbers[msg.sender];
+    }
+
+    function check() public view returns(uint) {
+        uint total;
+        uint length = numbers[tx.origin].length;
+
+        for(uint i = 0; i < length; i++) {
+            total += matching(numbers[tx.origin][i].number);
+        }
+
+        return total;
+    }
+
+    function matching(string memory _s) public view returns(uint) {
+        uint cnt;
+ 
+        for(uint i = 0; i < 6; i++) {
+            if(bytes(_s)[i] == bytes(target[target.length - 1].number)[i]) {
+                cnt++;
+            }
+        }
+
+        return distribute(cnt);
+    }
+
+    function distribute(uint _n) public pure returns(uint) {
+        if(_n == 6) {
+            return 10 ** 18;
+        } else if(_n == 5) {
+            return 75 * 10 ** 16;
+        } else if(_n == 4) {
+            return 25 * 10 ** 16;
+        } else if(_n == 3) {
+            return 10 ** 17;
+        } else {
+            return 0;
         }
     }
 }
